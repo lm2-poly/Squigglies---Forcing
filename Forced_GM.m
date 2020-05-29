77be019310c5752d52da9938ae7de9f764818d44
yapeg <63878318+yapeg@users.noreply.github.com>
2020-05-29 22:36:07 +0200

    Update Forced_GM.m

--- Forced_GM.m
+++ Forced_GM.m
@@ -1,60 +1,46 @@
 global Rc SR Uc Ay lambda
-SR = 3.5; FR = 3; LR = 5;
+SR = 3.5; FR = 3; LR = 5;                                                            % Speed, frequency and length ratios
 
-fc = 1; Rc = 1; Uc = 2*pi*fc*Rc;
-Vp = Uc/SR; fy = fc*FR; Ay = Rc*LR;
-                 
-lambda = Uc/fy;
+fc = 1; Rc = 1; Uc = 2*pi*fc*Rc;                                                     % Steady coiling frequency, radius and extruding speed
+Vp = Uc/SR; fy = fc*FR; Ay = Rc*LR;                                                  % Belt speed, forcing frequency and forcing amplitude
+                                                                                     % Note: FR is with respect to fc (See "Complete.m" for FR with respect to pattern frequency)
+lambda = Uc/fy;                                                                      % lambda is the total amount of material extruded within a perturbation cycle
 
-tspan = [0 1e3]; Y0 = [1 0 pi/2];
-options = odeset('RelTol', 1e-6, 'AbsTol', [1e-5 1e-5 1e-5], 'refine', 5);
-[t,Y] = ode23(@forcing, tspan, Y0, options);
-Yc = Ay*sin(2*pi*Uc*t/lambda);
+tspan = [0 1e3]; Y0 = [1 0 pi/2];                                                    % Temporal integration interval and initial conditions
+options = odeset('RelTol', 1e-6, 'AbsTol', [1e-5 1e-5 1e-5], 'refine', 5);           % ODE options
+[t,Y] = ode23(@forcing, tspan, Y0, options);                                         % ODE numerical resolution - FORCING
+Yc = Ay*sin(2*pi*Uc*t/lambda);                                                       % Sinusoidal forcing function
 
-X1 = Y(:,1).*cos(Y(:,2));
-Y1 = Y(:,1).*sin(Y(:,2)) + Yc;
-X2 = X1 + (Uc/SR).*(t(end) - t);
-Y2 = Y1;
+X1 = Y(:,1).*cos(Y(:,2)); X2 = X1 + (Uc/SR).*(t(end) - t);                           % X-coordinate contact point trace / deposited trace
+Y1 = Y(:,1).*sin(Y(:,2)) + Yc; Y2 = Y1;                                              % Y-coordinate contact point trace / deposited trace
 
-% Extra
+%% ---------------------------------------------------- OTHER CONSIDERATIONS -----------------------------------------------------------------------------------------------------
 fun = @(t) Vp*sqrt(1+(2*pi*Ay*fy/Vp*cos(2*pi*fy*t)).^2);
-lambda2 = integral(fun,0,1/fy);
+lambda2 = integral(fun,0,1/fy);                                                      % Regular sine wave arc length
 if LR*FR > pi/2
-status = 'WARNING: Uc < Uw'; else       % Coiling speed lower than average wave speed
+status = 'WARNING: Uc < Uw'; else                                                    % Coiling speed is lower than average wave speed
 status = '-'; end
 if lambda2 > lambda
-status2 = 'WARNING: E < lambda'; else   % Extruded material within a wave lower than the own wave arc length
+status2 = 'WARNING: E < lambda'; else                                                % Extruded material within a wave is lower than the own wave arc length
 status2 = '-'; end
 
-%% Plotting
-txt = cell(3,1); txt{1} = ['SR = ', num2str(SR)]; txt{2} = ['FR = ', num2str(FR)];
-                 txt{3} = ['LR = ', num2str(LR)];
-                 
-%fname = ['SR', num2str(SR), 'FR', num2str(FR), 'LR', num2str(LR)];
-%mkdir('Plots',fname)
-%fpath = fullfile(pwd,'Plots',fname);
+%% ---------------------------------------------------- PLOTTING -----------------------------------------------------------------------------------------------------
+n = round(length(Y0)*0.5):length(Y0); m = round(length(Y0)*0.9):length(Y0);          % cutting solution array allows to work on the steady spectrum, if any.
+
 figure(1)
-plot(X1/Rc,Y1/Rc)
+plot(X1(n)/Rc,Y1(n)/Rc)                                                              % x- and y-coordinates are normalized over Rc
 title('Sinusoidal movement - Plot of contact point trace')
-xlabel('X(s) (mm)', 'FontWeight', 'Bold')
-ylabel('Y(s) (mm)', 'FontWeight', 'Bold')
-axis([min(X1) max(X1), min(Y1) max(Y1)])
-text(min(X1)+0.04*(max(X1)-min(X1)), max(Y1)-0.10*(max(Y1)-min(Y1)), txt, 'FontWeight', 'Bold', 'FontSize', 9)
-grid
-%saveas(figure(1),fullfile(fpath,'plot1.fig'))
+xlabel('x(s)', 'FontWeight', 'Bold')
+ylabel('y(s)', 'FontWeight', 'Bold')
 
 figure(2)
-plot(X2/Rc,Y2/Rc,'-r')
+plot(X2(m)/Rc(m),Y2/Rc,'-r')                                                         % X- and Y-coordinates are normalized over Rc
 title('Sinusoidal movement - Plot of deposited trace')
-xlabel('X(s) (mm)', 'FontWeight', 'Bold')
-ylabel('Y(s) (mm)', 'FontWeight', 'Bold')
-axis([min(X2) max(X2), min(Y2) max(Y2)])
-text(min(X2)+0.04*(max(X2)-min(X2)), max(Y2)-0.10*(max(Y2)-min(Y2)), txt, 'FontWeight', 'Bold', 'FontSize', 9)
-grid
-%saveas(figure(2),fullfile(fpath,'plot2.fig'))
+xlabel('X(s)', 'FontWeight', 'Bold')
+ylabel('Y(s)', 'FontWeight', 'Bold')
 
-%%
-function dY = forcing(t,Y)
+%% ---------------------------------------------------- CALLED FUNCTIONS -----------------------------------------------------------------------------------------------------
+function dY = forcing(t,Y)                                                           % Y(1) = r, Y(2) = phi, Y(3) = theta
 global Rc SR Uc Ay lambda
 dY = zeros(3,1);
 b = 0.715;
